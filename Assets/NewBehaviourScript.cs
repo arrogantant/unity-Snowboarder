5f79f6e46d369bf53c32b93bfba2b8f8b721bdf1
Junseong <david991129@naver.com>
2023-03-05 18:56:53 +0900

    Surface Effector 2D

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("one");
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
     }
 }
