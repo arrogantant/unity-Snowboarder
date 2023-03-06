124404b1e33e897b39d85c338e39ae94a7d949c5
Junseong <david991129@naver.com>
2023-03-06 20:18:20 +0900

    Namespace & Thin Switching

--- Assets/CrashDetector.cs
+++ Assets/CrashDetector.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("hit my head");
+            SceneManager.LoadScene(0);
         }
     }
 }
--- Assets/FInishLine.cs
+++ Assets/FInishLine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FInishLine : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class FInishLine : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            Debug.Log("finish");
+            SceneManager.LoadScene(0);
         }
     }
 }
