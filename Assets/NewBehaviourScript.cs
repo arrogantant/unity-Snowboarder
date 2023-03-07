a59151f5d93aa2f0ad03a9523187affc741496bf
Junseong <david991129@naver.com>
2023-03-07 16:44:20 +0900

    FindObjectOfType

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
+
     void Update()
+    {
+        Method();
+        Speed();
+    }
+
+    void Speed() 
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                surfaceEffector2D.speed = boostSpeed;
+            }
+            else
+            {
+                surfaceEffector2D.speed = baseSpeed;
+            }
+        }
+    
+    void Method()
     {
         if(Input.GetKey(KeyCode.LeftArrow))
         {
